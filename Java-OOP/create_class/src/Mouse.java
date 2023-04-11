ccbaf6feb445dd3b4a27f50fd5c993e31441f33d
Joao Henrique <joaoadsistemas@gmail.com>
2023-04-11 19:08:52 -0300

    Sett / Get / Constructor

---
+++ Java-OOP/create_class/src/Mouse.java
@@ -0,0 +1,30 @@
+public class Mouse {
+
+
+    private double peso;
+    private int dpi;
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    ///
+
+    public int getDpi() {
+        return this.dpi;
+    }
+
+    public void setDpi(int dpi) {
+        this.dpi = dpi;
+    }
+
+
+
+
+    
+}
