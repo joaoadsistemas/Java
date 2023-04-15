ab915a79fa1787c540a44de25338e83d9686a1e4
Joao Henrique <joaoadsistemas@gmail.com>
2023-04-15 18:31:27 -0300

    Encapsulamento basic finished

--- Java-Pilares/Encapsulamento/src/App.java
+++ Java-Pilares/Encapsulamento/src/App.java
@@ -1,5 +1,16 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    public static void main(String[] args) {
+        
+        controleRemoto c = new controleRemoto();
+
+        c.ligar();
+        c.play();
+        c.ligarMudo();
+        c.desligarMudo();
+        c.maisVolume();
+        c.abrirMenu();
+
+
     }
 }
--- Java-Pilares/Encapsulamento/src/controleRemoto.java
+++ Java-Pilares/Encapsulamento/src/controleRemoto.java
@@ -1,20 +1,14 @@
-public class controleRemoto {
+public class controleRemoto implements controlador {
     
-    int volume;
-    boolean ligado;
-    boolean mudo;
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
 
     public controleRemoto() {
-        setVolume(50);
-        setLigado(false);
-        setMudo(true);
+        this.setVolume(50);
+        this.setLigado(false);
+        this.setTocando(false);
     }
-
-
-
-
-
-
 
     private int getVolume() {
         return this.volume;
@@ -32,13 +26,92 @@ public class controleRemoto {
         this.ligado = l;
     }
 
-    private boolean getMudo() {
-        return this.mudo;
+    private boolean getTocando() {
+        return this.tocando;
     }
 
-    private void setMudo(boolean m) {
-        this.mudo = m;
+    private void setTocando(boolean m) {
+        this.tocando = m;
     }
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("-----MENU-----");
+       System.out.println("Está ligado? " + this.getLigado());
+       System.out.println("Está tocando? " + this.getTocando());
+       System.out.println("Volume: " + this.getVolume());
+
+       for (int i = 1; i <= this.getVolume(); i+= 10) {
+            if (getVolume() == 0) {
+                System.out.print(" o");
+            } else {
+                System.out.print(" |");
+            }
+        
+        }
+
+
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando menu...");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() + 5);
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() - 5);
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.getLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.getLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.getLigado() && !(this.getTocando())) {
+            this.setTocando(true);
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.getLigado() && this.getTocando()) {
+            this.setTocando(false);
+        }
+    }
+
+
+
 
 
 }
